eac041f71f5861d158c116a9a0408efd9202561d
lijith-mk <lijithmk357@gmail.com>
2025-10-24 21:57:22 +0530

    Add Admin Login link to footer

--- jobzee-frontend/src/components/Footer.jsx
+++ jobzee-frontend/src/components/Footer.jsx
@@ -138,6 +138,14 @@ const Footer = () => {
                   Blog & Resources
                 </Link>
               </li>
+              <li>
+                <Link to="/admin/login" className="text-gray-300 hover:text-yellow-400 transition-colors duration-200 flex items-center">
+                  <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                  </svg>
+                  Admin Login
+                </Link>
+              </li>
             </ul>
           </div>
 
